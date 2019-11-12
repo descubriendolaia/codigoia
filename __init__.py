9f3ec59d99d0076e378ccb5ba575a103fd8536aa
JL Iglesias Feria <jl.iglesias.feria@protonmail.com>
2019-11-12 20:37:26 +0100

    Comentarios en archivos '__init__.py' de los módulos

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+"""
+Inicialización del módulo de código del canal de Youtube 'Descubriendo la
+Inteligencia Artificial'.
+"""
--- busqueda/__init__.py
+++ busqueda/__init__.py
@@ -1,2 +1,5 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+"""
+Inicialización del módulo de búsquedas en grafos.
+"""
